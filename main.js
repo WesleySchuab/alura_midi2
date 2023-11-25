0948c39bb84c7e31bc900c365c70a4ae7d5d25a6
Wesley Schuab <yinnihisoka@gmail.com>
2023-11-25 17:36:35 -0300

    Adicionando link para o javascritp

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-function tocaSomPom()
-{
+function tocaSomPom() {
     //document defini o escopo como sendo o documento. html
     // querySelector seleciona um elemento HTML
     // # seleciona apartir do id
-    document.querySelector('#som_tecla_pom').play()
+    document.querySelector('#som_tecla_pom').play();
 }
 // Quando a telca _pom é clicada chama a função tocaSomPom 
-document.querySelector('.tecla_pom').onclick= tocaSomPom;
+// document.querySelector('.tecla_pom').onclick= tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
