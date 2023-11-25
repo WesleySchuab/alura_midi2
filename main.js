7fc1f1443d599f21022be8e65a430cd9b9284a53
Wesley Schuab <yinnihisoka@gmail.com>
2023-11-25 17:58:23 -0300

    laco que atribui função as teclas

--- main.js
+++ main.js
@@ -5,7 +5,21 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 // Quando a telca _pom é clicada chama a função tocaSomPom 
+// codigo em desuso
 // document.querySelector('.tecla_pom').onclick= tocaSomPom;
+
+// constante que ira armazenar em uma lsita a referencia para as teclas
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+// Exemplod euso de uma tecla indexida 
+// listaDeTeclas[0].onclick = tocaSomPom;
+
+// criando uma variavel para controlar o laço 
+let contador = 0;
+//contador que vai atribuir a função a cada tecla 
+while(contador <listaDeTeclas.length)
+{
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador++;
+    console.log(contador);
+}
