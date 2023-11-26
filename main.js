3eff34263c1d2244eea745824c7f31fe15ab6dc7
Wesley Schuab <yinnihisoka@gmail.com>
2023-11-26 07:05:06 -0300

    otimizando codigo com for

--- main.js
+++ main.js
@@ -5,21 +5,20 @@ function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 // Quando a telca _pom é clicada chama a função tocaSomPom 
-// codigo em desuso
 // document.querySelector('.tecla_pom').onclick= tocaSomPom;
 
 // constante que ira armazenar em uma lista a referencia para as teclas
- const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 // Exemplod euso de uma tecla indexida 
-// codigo em desuso
 // listaDeTeclas[0].onclick = tocaSomPom;
 
 // criando uma variavel para controlar o laço 
+/*
 let contador = 0;
 
 //contador que vai atribuir a função a cada tecla 
-while (contador < listaDeTeclas.length) {
+ while (contador < listaDeTeclas.length) {
     //chama a função toca som quando uma tecla é clicada
     // Em desuso
     // listaDeTeclas[contador].onclick = tocaSomPom;
@@ -41,4 +40,13 @@ while (contador < listaDeTeclas.length) {
 
     contador++;
     console.log(contador);
+}
+ */
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 }
